48e0dd10167838d444ec945e6403469c75114fbd
birgerjoergensen-hue <birger.joergensen@gmail.com>
2026-05-31 00:58:45 +0200

    Update ANT2.ino

--- ANT2.ino
+++ ANT2.ino
@@ -1,5 +1,5 @@
 // =================================================================
-// PROJEKT: Birger DIY 54 (Timing & Bluetooth Start-Fix)
+// PROJEKT: Birger DIY 56 (Buchstaben-Test)
 // =================================================================
 #include <bluefruit.h>
 #include <hal/nrf_gpio.h>
@@ -7,32 +7,23 @@
 BLEDis bledis;
 BLEHidAdafruit blehid;
 
-#define PIN_D8       NRF_GPIO_PIN_MAP(1, 4)
-#define PIN_D9       NRF_GPIO_PIN_MAP(1, 6)
-#define RED_LED      NRF_GPIO_PIN_MAP(0, 15)
-
-const bool INVERT_LED = true; 
-
-void setLed(bool on) {
-  nrf_gpio_pin_write(RED_LED, INVERT_LED ? !on : on);
-}
+#define PIN_D8_PHYSICAL  NRF_GPIO_PIN_MAP(1, 4)
+#define PIN_D9_PHYSICAL  NRF_GPIO_PIN_MAP(1, 6)
+#define RED_LED          NRF_GPIO_PIN_MAP(0, 15)
 
 void setup() {
-  // WICHTIG: Bluetooth Zeit zum Starten geben
-  delay(3000); 
-
   Bluefruit.begin();
   Bluefruit.setAppearance(BLE_APPEARANCE_HID_KEYBOARD);
   
   bledis.begin();
   blehid.begin();
   
-  Bluefruit.setName("Birger DIY 54");
+  Bluefruit.setName("Birger DIY 56");
   
-  nrf_gpio_cfg_input(PIN_D9, NRF_GPIO_PIN_PULLUP);
-  nrf_gpio_cfg_input(PIN_D8, NRF_GPIO_PIN_PULLUP);
+  nrf_gpio_cfg_input(PIN_D9_PHYSICAL, NRF_GPIO_PIN_PULLUP);
+  nrf_gpio_cfg_input(PIN_D8_PHYSICAL, NRF_GPIO_PIN_PULLUP);
   nrf_gpio_cfg_output(RED_LED);
-  setLed(false); 
+  nrf_gpio_pin_write(RED_LED, 0); 
 
   Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
   Bluefruit.Advertising.addService(blehid);
@@ -42,22 +33,24 @@ void setup() {
 }
 
 void loop() {
-  if (nrf_gpio_pin_read(PIN_D9) == 0) {
-    setLed(true);
+  // D9 sendet 'A'
+  if (nrf_gpio_pin_read(PIN_D9_PHYSICAL) == 0) {
+    nrf_gpio_pin_write(RED_LED, 1);
     blehid.keyPress(HID_KEY_A);
     delay(50);
     blehid.keyRelease();
-    while (nrf_gpio_pin_read(PIN_D9) == 0) { delay(50); }
-    setLed(false);
+    while (nrf_gpio_pin_read(PIN_D9_PHYSICAL) == 0) { delay(10); }
+    nrf_gpio_pin_write(RED_LED, 0);
   }
 
-  if (nrf_gpio_pin_read(PIN_D8) == 0) {
-    setLed(true);
-    blehid.keyPress(HID_KEY_ARROW_DOWN);
+  // D8 sendet 'B' (statt Pfeiltaste runter)
+  if (nrf_gpio_pin_read(PIN_D8_PHYSICAL) == 0) {
+    nrf_gpio_pin_write(RED_LED, 1);
+    blehid.keyPress(HID_KEY_B);
     delay(50);
     blehid.keyRelease();
-    while (nrf_gpio_pin_read(PIN_D8) == 0) { delay(50); }
-    setLed(false);
+    while (nrf_gpio_pin_read(PIN_D8_PHYSICAL) == 0) { delay(10); }
+    nrf_gpio_pin_write(RED_LED, 0);
   }
-  delay(50);
+  delay(10);
 }
