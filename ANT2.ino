defe1250280566e3202083d38f46fc77707c7a38
birgerjoergensen-hue <birger.joergensen@gmail.com>
2026-05-30 22:39:02 +0200

    Update ANT2.ino

--- ANT2.ino
+++ ANT2.ino
@@ -22,7 +22,7 @@ void startAdv(void) {
   Bluefruit.Advertising.restartOnDisconnect(true);
   Bluefruit.Advertising.setInterval(32, 244);
   Bluefruit.Advertising.setFastTimeout(30);
-  Bluefruit.Advertising.start(0);
+  Bluefruit.Advertising.start(0); // 0 = Unendlich senden, bis Verbindung steht
 }
 
 void pairing_complete_callback(uint16_t conn_handle, uint8_t auth_status) {
@@ -53,7 +53,6 @@ void tapKey(uint8_t keycode) {
   if (!Bluefruit.connected()) {
     return;
   }
-
   blehid.keyPress(keycode);
   delay(50);
   blehid.keyRelease();
@@ -69,3 +68,48 @@ void setup() {
   pinMode(ANT1, INPUT_PULLUP);
   pinMode(ANT2, INPUT_PULLUP);
   pinMode(ANT3, INPUT_PULLUP);
+  pinMode(ANT4, INPUT_PULLUP);
+
+  Bluefruit.begin();
+  Bluefruit.setTxPower(4);
+  
+  // IMMER WEITERZÄHLEN: Version 19 scharf geschaltet!
+  Bluefruit.setName("Birger DIY 19");
+
+  // HIER DIE WICHTIGE ERGÄNZUNG: Deine Callbacks müssen dem System auch zugewiesen werden!
+  Bluefruit.Periph.setConnectCallback(connect_callback);
+  Bluefruit.Periph.setDisconnectCallback(disconnect_callback);
+  Bluefruit.Security.setPairingCompleteCallback(pairing_complete_callback);
+  Bluefruit.Security.setSecuredCallback(connection_secured_callback);
+
+  bledis.setManufacturer("GEMMI Tech");
+  bledis.setModel("Blipbox v4");
+  bledis.begin();
+
+  blehid.begin();
+
+  startAdv();
+}
+
+void loop() {
+  // Knöpfe abfragen und deine "tapKey"-Funktion nutzen
+  if (digitalRead(ANT1) == LOW) {
+    tapKey(HID_KEY_ARROW_LEFT);
+    waitForRelease(ANT1);
+  }
+
+  if (digitalRead(ANT2) == LOW) {
+    tapKey(HID_KEY_ARROW_RIGHT);
+    waitForRelease(ANT2);
+  }
+
+  if (digitalRead(ANT3) == LOW) {
+    waitForRelease(ANT3);
+  }
+
+  if (digitalRead(ANT4) == LOW) {
+    waitForRelease(ANT4);
+  }
+
+  delay(10);
+}
