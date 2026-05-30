3a303092a8a99b6f66ec744757b73534ff2e755b
birgerjoergensen-hue <birger.joergensen@gmail.com>
2026-05-30 22:33:03 +0200

    Update ANT2.ino

--- ANT2.ino
+++ ANT2.ino
@@ -8,8 +8,7 @@ const int ANT2 = 10;
 const int ANT3 = 16;
 const int ANT4 = 8;
 
-void startAdv(void)
-{
+void startAdv(void) {
   Bluefruit.Advertising.stop();
   Bluefruit.Advertising.clearData();
   Bluefruit.ScanResponse.clearData();
@@ -26,22 +25,8 @@ void startAdv(void)
   Bluefruit.Advertising.start(0);
 }
 
-void connect_callback(uint16_t conn_handle)
-{
-  (void) conn_handle;
-}
-
-void disconnect_callback(uint16_t conn_handle, uint8_t reason)
-{
-  (void) conn_handle;
-  (void) reason;
-  startAdv();
-}
-
-void tapKey(uint8_t keycode)
-{
-  if (!Bluefruit.connected())
-  {
+void tapKey(uint8_t keycode) {
+  if (!Bluefruit.connected()) {
     return;
   }
 
@@ -50,16 +35,13 @@ void tapKey(uint8_t keycode)
   blehid.keyRelease();
 }
 
-void waitForRelease(int pin)
-{
-  while (digitalRead(pin) == LOW)
-  {
+void waitForRelease(int pin) {
+  while (digitalRead(pin) == LOW) {
     delay(10);
   }
 }
 
-void setup()
-{
+void setup() {
   pinMode(ANT1, INPUT_PULLUP);
   pinMode(ANT2, INPUT_PULLUP);
   pinMode(ANT3, INPUT_PULLUP);
@@ -67,6 +49,37 @@ void setup()
 
   Bluefruit.begin();
   Bluefruit.setTxPower(4);
-  Bluefruit.setName("Birger DIY 16");
+  Bluefruit.setName("Birger DIY 17");
 
-  Bluefruit.Periph.setConnectCallback(connect_callback);
+  bledis.setManufacturer("GEMMI Tech");
+  bledis.setModel("Blipbox v4");
+  bledis.begin();
+
+  blehid.begin();
+
+  startAdv();
+}
+
+void loop() {
+  if (digitalRead(ANT1) == LOW) {
+    tapKey(HID_KEY_ARROW_LEFT);
+    waitForRelease(ANT1);
+  }
+
+  if (digitalRead(ANT2) == LOW) {
+    tapKey(HID_KEY_ARROW_RIGHT);
+    waitForRelease(ANT2);
+  }
+
+  if (digitalRead(ANT3) == LOW) {
+    tapKey(HID_KEY_ARROW_UP);
+    waitForRelease(ANT3);
+  }
+
+  if (digitalRead(ANT4) == LOW) {
+    tapKey(HID_KEY_ARROW_DOWN);
+    waitForRelease(ANT4);
+  }
+
+  delay(10);
+}
