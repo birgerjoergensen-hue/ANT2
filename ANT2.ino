e6c02383ca8da1ab415d25df7cd14e715ca5ef01
birgerjoergensen-hue <birger.joergensen@gmail.com>
2026-05-30 22:56:14 +0200

    Remove unused Bluetooth advertising setup code

--- ANT2.ino
+++ ANT2.ino
@@ -1,3 +1,7 @@
+// =================================================================
+// PROJEKT: Blipbox v4 - VERSION: Birger DIY 24
+// =================================================================
+
 #include <bluefruit.h>
 
 BLEDis bledis;
@@ -14,106 +18,4 @@ void startAdv(void) {
   Bluefruit.ScanResponse.clearData();
 
   Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
-  Bluefruit.Advertising.addTxPower();
-  Bluefruit.Advertising.addAppearance(BLE_APPEARANCE_HID_KEYBOARD);
-  Bluefruit.Advertising.addService(blehid);
-  Bluefruit.Advertising.addName();
-
-  Bluefruit.Advertising.restartOnDisconnect(true);
-  Bluefruit.Advertising.setInterval(32, 244);
-  Bluefruit.Advertising.setFastTimeout(30);
-  Bluefruit.Advertising.start(0); 
-}
-
-void pairing_complete_callback(uint16_t conn_handle, uint8_t auth_status) {
-  (void) conn_handle;
-  (void) auth_status;
-}
-
-void connection_secured_callback(uint16_t conn_handle) {
-  (void) conn_handle;
-}
-
-void connect_callback(uint16_t conn_handle) {
-  BLEConnection* connection = Bluefruit.Connection(conn_handle);
-  if (connection != NULL) {
-    if (!connection->secured()) {
-      connection->requestPairing();
-    }
-  }
-}
-
-void disconnect_callback(uint16_t conn_handle, uint8_t reason) {
-  (void) conn_handle;
-  (void) reason;
-  startAdv();
-}
-
-void tapKey(uint8_t keycode) {
-  if (!Bluefruit.connected()) {
-    return;
-  }
-  blehid.keyPress(keycode);
-  delay(50);
-  blehid.keyRelease();
-}
-
-void waitForRelease(int pin) {
-  while (digitalRead(pin) == LOW) {
-    delay(10);
-  }
-}
-
-void setup() {
-  pinMode(ANT1, INPUT_PULLUP);
-  pinMode(ANT2, INPUT_PULLUP);
-  pinMode(ANT3, INPUT_PULLUP);
-  pinMode(ANT4, INPUT_PULLUP);
-
-  Bluefruit.begin();
-  Bluefruit.setTxPower(4);
-  
-  // HIER DIE ENTSCHEIDENDE ERGÄNZUNG FÜR VERSION 21:
-  // Wir sagen dem nRF52-Chip, dass er Verschlüsselung ohne Display (Just Works) beherrscht.
-  // Ohne diese Zeile verweigert das Handy nach dem requestPairing() die dauerhafte Verbindung!
-  Bluefruit.Security.setIOCaps(MITM_IOCAPS_NONE); 
-
-  // STUR HOCHGEZÄHLT: Version 21 gegen den Cache
-  Bluefruit.setName("Birger DIY 21");
-
-  // Deine Callbacks sauber registriert
-  Bluefruit.Periph.setConnectCallback(connect_callback);
-  Bluefruit.Periph.setDisconnectCallback(disconnect_callback);
-  Bluefruit.Security.setPairCompleteCallback(pairing_complete_callback);
-  Bluefruit.Security.setSecuredCallback(connection_secured_callback);
-
-  bledis.setManufacturer("GEMMI Tech");
-  bledis.setModel("Blipbox v4");
-  bledis.begin();
-
-  blehid.begin();
-
-  startAdv();
-}
-
-void loop() {
-  if (digitalRead(ANT1) == LOW) {
-    tapKey(HID_KEY_ARROW_LEFT);
-    waitForRelease(ANT1);
-  }
-
-  if (digitalRead(ANT2) == LOW) {
-    tapKey(HID_KEY_ARROW_RIGHT);
-    waitForRelease(ANT2);
-  }
-
-  if (digitalRead(ANT3) == LOW) {
-    waitForRelease(ANT3);
-  }
-
-  if (digitalRead(ANT4) == LOW) {
-    waitForRelease(ANT4);
-  }
-
-  delay(10);
-}
+  Bluefruit.
