501a10622609180c02318cae5ac88bfafb416154
birgerjoergensen-hue <birger.joergensen@gmail.com>
2026-05-31 10:14:46 +0200

    Update ANT2.ino

--- ANT2.ino
+++ ANT2.ino
@@ -1,34 +1,33 @@
 #include <bluefruit.h>
 
 BLEDis bledis;
-BLEHeartRate blehr; 
+BLEHrm blehrm; // Profil für Herzfrequenz-Sensor
 
 void setup() {
   Bluefruit.begin();
   
-  // Identität
-  bledis.setManufacturer("Birger");
+  // Ich habe die Versionierung für dich übernommen:
+  Bluefruit.setName("GEMMI_31v10");
+  
+  bledis.setManufacturer("GEMMI DIY");
   bledis.setModel("ANT-Bridge");
   bledis.begin();
 
-  // Herzfrequenz-Profil (HRM) - Das ist der "Universal-Türöffner" für Radcomputer
-  blehr.begin();
+  blehrm.begin();
   
-  Bluefruit.setName("Birger_31v09");
-  
-  // Wichtig für Coros/Handy: Explizites Advertising
+  // Sichtbarkeit für das Coros Dura (als Sensor)
   Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
-  Bluefruit.Advertising.addService(blehr);
+  Bluefruit.Advertising.addService(blehrm);
   Bluefruit.Advertising.addName();
   
-  // Schnellere Wiederholung für bessere Erkennung
+  // Schnelles Senden für den Suchmodus
   Bluefruit.Advertising.setInterval(32, 244); 
   Bluefruit.Advertising.setFastTimeout(30);
   Bluefruit.Advertising.start(0);
 }
 
 void loop() {
-  // Simuliere einen Herzschlag von 80 BPM, damit das Gerät "aktiv" wirkt
-  blehr.notify(80);
+  // Simulierter Puls von 80 BPM
+  blehrm.measure(80); 
   delay(1000);
 }
