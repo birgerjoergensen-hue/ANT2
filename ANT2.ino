70b9c3e927a1e9b314ae9828b3219747baba1d78
birgerjoergensen-hue <birger.joergensen@gmail.com>
2026-05-31 08:40:25 +0200

    Update ANT2.ino

--- ANT2.ino
+++ ANT2.ino
@@ -1,24 +1,5 @@
-// =================================================================
-// PROJEKT: Birger DIY 31v07 (Offene UART-Brücke)
-// =================================================================
-#include <bluefruit.h>
-
-BLEUart bleuart; 
-
 void setup() {
-  Bluefruit.begin();
-  Bluefruit.setName("Birger DIY 31v07");
-  
-  // UART-Service ist der "Türöffner" für fast alle Systeme
-  bleuart.begin();
-  
-  Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
-  Bluefruit.Advertising.addService(bleuart);
-  Bluefruit.Advertising.addName();
-  Bluefruit.Advertising.start(0);
+  // Absolut leer, kein Bluetooth, keine Initialisierung
 }
-
 void loop() {
-  // Kein Code, wir wollen nur die stabile Verbindung
-  delay(1000);
 }
