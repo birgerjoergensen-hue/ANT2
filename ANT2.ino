3efecf62ac1e1211e4ccf9d7421fa84de12eaf06
birgerjoergensen-hue <birger.joergensen@gmail.com>
2026-05-31 08:07:17 +0200

    Update ANT2.ino

--- ANT2.ino
+++ ANT2.ino
@@ -1,10 +1,10 @@
 // =================================================================
-// PROJEKT: Birger DIY 31v04 (Rad-Sensor Profil)
+// PROJEKT: Birger DIY 31v05 (Fix für Sensor-Profil)
 // =================================================================
 #include <bluefruit.h>
 
 BLEDis bledis;
-BLECSCS blecsc; // Cycling Speed & Cadence Profil
+BLECscs blecsc; // Die Klasse schreibt sich BLECscs (C für Cycling, s für Speed, c für Cadence, s für Service)
 
 void setup() {
   Bluefruit.begin();
@@ -13,10 +13,12 @@ void setup() {
   bledis.setModel("ANT-Bridge");
   bledis.begin();
 
+  // Initialisiere den Cycling Speed & Cadence Service
   blecsc.begin();
   
-  Bluefruit.setName("Birger DIY 31v04");
+  Bluefruit.setName("Birger DIY 31v05");
   
+  // Advertising
   Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
   Bluefruit.Advertising.addService(blecsc);
   Bluefruit.Advertising.addName();
@@ -24,6 +26,5 @@ void setup() {
 }
 
 void loop() {
-  // Wir senden keine Daten, nur das Profil-Signal
   delay(1000);
 }
