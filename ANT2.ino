6de94825108c7c36d958732504ff8b97f7286e65
birgerjoergensen-hue <birger.joergensen@gmail.com>
2026-05-31 09:32:51 +0200

    Update ANT2.ino v9

--- ANT2.ino
+++ ANT2.ino
@@ -1,19 +1,34 @@
 #include <bluefruit.h>
 
+BLEDis bledis;
+BLEHeartRate blehr; 
+
 void setup() {
-  // Starte Bluetooth
   Bluefruit.begin();
   
-  // Setze einen eindeutigen Namen, den du sofort wiedererkennst
-  Bluefruit.setName("Birger_31v08");
+  // Identität
+  bledis.setManufacturer("Birger");
+  bledis.setModel("ANT-Bridge");
+  bledis.begin();
+
+  // Herzfrequenz-Profil (HRM) - Das ist der "Universal-Türöffner" für Radcomputer
+  blehr.begin();
   
-  // Advertising starten (Broadcast-Modus)
+  Bluefruit.setName("Birger_31v09");
+  
+  // Wichtig für Coros/Handy: Explizites Advertising
   Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
+  Bluefruit.Advertising.addService(blehr);
   Bluefruit.Advertising.addName();
+  
+  // Schnellere Wiederholung für bessere Erkennung
+  Bluefruit.Advertising.setInterval(32, 244); 
+  Bluefruit.Advertising.setFastTimeout(30);
   Bluefruit.Advertising.start(0);
 }
 
 void loop() {
-  // Nichts tun, einfach nur sichtbar bleiben
+  // Simuliere einen Herzschlag von 80 BPM, damit das Gerät "aktiv" wirkt
+  blehr.notify(80);
   delay(1000);
 }
