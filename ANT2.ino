69cf5670084c0a964a9b137b2d6745b1af733f23
birgerjoergensen-hue <birger.joergensen@gmail.com>
2026-06-01 13:05:39 +0200

    Update BLE HID device settings and functionality

--- ANT2.ino
+++ ANT2.ino
@@ -1,46 +1,49 @@
 /* * BLIPBOX-V35 
- * Version: 2026-06-01-V1 
- * BLE-E-Bike-Steuerung für Coros Dura
+ * Version: 2026-06-01-V2
+ * HID-Test-Modus (5s Intervall)
  */
 
 #include <bluefruit.h>
 
-BLEService        ebs = BLEService(0x183C); 
-BLECharacteristic ebc = BLECharacteristic(0x2B56); 
+// Globale HID-Objekte
+BLEDis       bledis;
+BLEHidAdafruit blehid;
 
 void setup() {
   Bluefruit.begin();
   Bluefruit.setTxPower(4);
-  Bluefruit.setName("E-BIKE-STEPS");
+  Bluefruit.setName("BLIPBOX-HID-TEST");
 
-  ebs.begin();
-  ebc.setProperties(CHR_PROPS_NOTIFY | CHR_PROPS_WRITE);
-  ebc.setFixedLen(2);
-  ebc.begin();
+  // Geräteinfo für das Pairing
+  bledis.setManufacturer("Adafruit Industries");
+  bledis.setModel("Blipbox V35");
+  bledis.begin();
 
-  // Physikalische Pin-Map für Feather nRF52840 Sense
-  pinMode(NRF_GPIO_PIN_MAP(1, 6), INPUT_PULLUP);
+  // HID-Dienst initialisieren
+  blehid.begin();
 
+  // Advertising-Einstellungen für ein HID-Gerät
   Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
   Bluefruit.Advertising.addTxPower();
-  Bluefruit.Advertising.addAppearance(0x044C); 
-  Bluefruit.Advertising.addService(ebs);
+  Bluefruit.Advertising.addAppearance(BLE_APPEARANCE_HID_KEYBOARD);
+  Bluefruit.Advertising.addService(blehid);
   Bluefruit.Advertising.addName();
+  
+  // Wiederverbinden ermöglichen
   Bluefruit.Advertising.restartOnDisconnect(true);
   Bluefruit.Advertising.start(0);
 }
 
-++
 void loop() {
   if (Bluefruit.connected()) {
-    // 0x4B ist Page Down, 0 ist für "kein Modifier (Shift/Ctrl)"
+    // 0x4B ist Page Down, 0 ist der Modifier (Shift/Ctrl etc.)
+    // Die Signatur blehid.keyPress(keycode, modifier) ist hier zwingend
     blehid.keyPress(0x4B, 0); 
     delay(100);
     blehid.keyRelease();
     
+    // Warte 5 Sekunden bis zum nächsten "Tastendruck"
     delay(5000); 
   }
   delay(100);
-}
-
 }
