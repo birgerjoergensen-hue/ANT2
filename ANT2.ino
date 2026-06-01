f40724e09c94048736a9c5b9aad19f628d0ec27c
birgerjoergensen-hue <birger.joergensen@gmail.com>
2026-06-01 08:03:24 +0200

    Update ANT2.ino

--- ANT2.ino
+++ ANT2.ino
@@ -1,15 +1,36 @@
-#include <Arduino.h>
+#include <bluefruit.h>
+
+BLEService        hrms = BLEService(UUID16_SVC_HEART_RATE);
+BLECharacteristic hrmc = BLECharacteristic(UUID16_CHR_HEART_RATE_MEASUREMENT);
 
 void setup() {
-  pinMode(LED_BLUE, OUTPUT);
-  pinMode(LED_RED, OUTPUT);
+  Bluefruit.begin();
+  Bluefruit.setTxPower(4);
+  Bluefruit.setName("BLIPBOX-V35");
+
+  // Herzfrequenz-Service aufsetzen
+  hrms.begin();
+  hrmc.setProperties(CHR_PROPS_NOTIFY);
+  hrmc.setFixedLen(2);
+  hrmc.begin();
+
+  // Advertising-Daten für den Coros Dura
+  Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
+  Bluefruit.Advertising.addTxPower();
+  Bluefruit.Advertising.addService(hrms);
+  Bluefruit.Advertising.addName();
   
-  digitalWrite(LED_BLUE, LOW);
-  digitalWrite(LED_RED, HIGH);
-  delay(1000);
-  digitalWrite(LED_RED, LOW);
+  Bluefruit.Advertising.restartOnDisconnect(true);
+  Bluefruit.Advertising.setInterval(32, 244); // 20ms bis 150ms
+  Bluefruit.Advertising.setFastTimeout(30);
+  Bluefruit.Advertising.start(0); // Dauerhaftes Advertising
 }
 
 void loop() {
-  __WFI(); 
+  if (Bluefruit.Connected()) {
+    // Sende Dummy-Herzfrequenz (72 BPM) als Hex-Daten: 00 48
+    uint8_t hrmData[] = {0x00, 0x48}; 
+    hrmc.notify(hrmData, sizeof(hrmData));
+  }
+  delay(1000);
 }
