77cdb88a57516f7a29396ea9a083f0eb9025a927
birgerjoergensen-hue <birger.joergensen@gmail.com>
2026-06-01 21:43:10 +0200

    Update ANT2.ino

--- ANT2.ino
+++ ANT2.ino
@@ -1,16 +1,17 @@
 /* * BLIPBOX-V35 
- * Version: 2026-06-01-V10
- * Strategie: HID Gamepad mit korrekter Methode: gamepadReport
+ * Version: 2026-06-01-V12
+ * Strategie: Zurück zum stabilen Keyboard-Profil (HID)
  */
 
 #include <bluefruit.h>
 
 BLEDis       bledis;
-BLEHidGamepad blehid;
+BLEHidAdafruit blehid;
 
 void setup() {
   Bluefruit.begin();
-  Bluefruit.setName("BLIPBOX-REMOTE");
+  // Name so gewählt, dass er als Gerät erkannt wird
+  Bluefruit.setName("BLIPBOX-V35");
 
   bledis.setManufacturer("Adafruit Industries");
   bledis.setModel("Blipbox V35");
@@ -22,7 +23,7 @@ void setup() {
 
   Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
   Bluefruit.Advertising.addTxPower();
-  Bluefruit.Advertising.addAppearance(0x0540); 
+  Bluefruit.Advertising.addAppearance(BLE_APPEARANCE_HID_KEYBOARD);
   Bluefruit.Advertising.addService(blehid);
   Bluefruit.Advertising.addName();
   
@@ -34,19 +35,13 @@ void loop() {
   if (Bluefruit.connected()) {
     if (digitalRead(NRF_GPIO_PIN_MAP(1, 6)) == LOW) {
       
-      // Korrekt: gamepadReport nimmt die Struktur entgegen
-      hid_gamepad_report_t report = {
-        .x = 0, .y = 0, .z = 0, .rz = 0, .rx = 0, .ry = 0,
-        .hat = 0, .buttons = 1
-      };
-      blehid.gamepadReport(&report);
-      delay(100);
+      // Anstatt Page Down senden wir "Enter" (0x28). 
+      // Viele Radcomputer nutzen Enter/Select als Bestätigung oder Seitenwechsel
+      blehid.keyPress(0x28, 0); 
+      delay(50);
+      blehid.keyRelease();
       
-      // Report mit Buttons = 0 senden
-      report.buttons = 0;
-      blehid.gamepadReport(&report);
-      
-      delay(500); 
+      delay(1000); // Längere Pause
     }
   }
   delay(100);
