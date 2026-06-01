81e0a0138df9657f6e498aa2f8d8685fcbee4295
birgerjoergensen-hue <birger.joergensen@gmail.com>
2026-06-01 07:15:50 +0200

    Simplify setup and loop functions in ANT2.ino
    Refactor setup and loop functions for clarity and efficiency.

--- ANT2.ino
+++ ANT2.ino
@@ -1,33 +1,18 @@
 #include <Arduino.h>
 
 void setup() {
+  // Pin-Definitionen für Feather nRF52840 Sense
   pinMode(LED_BLUE, OUTPUT);
   pinMode(LED_RED, OUTPUT);
   
-  digitalWrite(LED_BLUE, LOW);
-  digitalWrite(LED_RED, HIGH);
+  // Status: System startet
+  digitalWrite(LED_BLUE, LOW);  // BLE aus
+  digitalWrite(LED_RED, HIGH);  // Rot an als Lebenszeichen
   delay(1000);
   digitalWrite(LED_RED, LOW);
 }
 
 void loop() {
-  __WFI(); 
-}
-#include <Arduino.h>
-
-void setup() {
-  pinMode(LED_BLUE, OUTPUT);
-  pinMode(LED_RED, OUTPUT);
-  
-  // Blaue LED (BLE-Status) hart auf LOW
-  digitalWrite(LED_BLUE, LOW);
-  
-  // Rote LED kurz an, um Lebenszeichen zu geben
-  digitalWrite(LED_RED, HIGH);
-  delay(1000);
-  digitalWrite(LED_RED, LOW);
-}
-
-void loop() {
+  // Minimaler Loop, um den Prozessor aktiv zu halten
   __WFI(); 
 }
