10dd0e8a720c519567e8a87cc9c4920c264eaa06
birgerjoergensen-hue <birger.joergensen@gmail.com>
2026-06-01 07:30:35 +0200

    Refactor LED initialization in setup function

--- ANT2.ino
+++ ANT2.ino
@@ -1,18 +1,15 @@
 #include <Arduino.h>
 
 void setup() {
-  // Pin-Definitionen für Feather nRF52840 Sense
   pinMode(LED_BLUE, OUTPUT);
   pinMode(LED_RED, OUTPUT);
   
-  // Status: System startet
-  digitalWrite(LED_BLUE, LOW);  // BLE aus
-  digitalWrite(LED_RED, HIGH);  // Rot an als Lebenszeichen
+  digitalWrite(LED_BLUE, LOW);
+  digitalWrite(LED_RED, HIGH);
   delay(1000);
   digitalWrite(LED_RED, LOW);
 }
 
 void loop() {
-  // Minimaler Loop, um den Prozessor aktiv zu halten
   __WFI(); 
 }
