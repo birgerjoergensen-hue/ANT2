a5e854fc34fc2d8b604085d759b80b4a2e17b04e
birgerjoergensen-hue <birger.joergensen@gmail.com>
2026-06-01 06:46:23 +0200

    Update ANT2.ino

--- ANT2.ino
+++ ANT2.ino
@@ -1,20 +1,15 @@
 #include <Arduino.h>
 
 void setup() {
-  // Wir nutzen die Namen, die das Board von Haus aus kennt
   pinMode(LED_BLUE, OUTPUT);
   pinMode(LED_RED, OUTPUT);
   
-  // Blaue LED (BLE-Status) hart auf LOW
   digitalWrite(LED_BLUE, LOW);
-  
-  // Rote LED (System-Status) kurz an
   digitalWrite(LED_RED, HIGH);
   delay(1000);
   digitalWrite(LED_RED, LOW);
 }
 
 void loop() {
-  // Warteschleife
   __WFI(); 
 }
